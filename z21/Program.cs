373a861693d4653c2dcaf8d7afd38b9e02b5ead9
Ksusha <ksushalive@yandex.ru>
2022-11-28 21:28:38 +0300

    init: solution of 21 task

---
+++ z21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первую координату первого числа");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату первого числа");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третью координату первого числа");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату второго числа");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату второго числа");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третью координату второго числа");
+double z2 = double.Parse(Console.ReadLine());
+
+double otv = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками равно {otv}");
